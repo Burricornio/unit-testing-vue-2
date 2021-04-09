aa2470b3c641bee9085ce189b225794e91d4fbf9
Burricornio <alejandro_web@hotmail.com>
2021-04-09 13:55:53 +0200

    Lesson2 - Testing Data

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AppHeader extends Vue {
+  loggedIn = false
+}
+</script>
+
+<style scoped></style>
---
+++ tests/unit/AppHeader.spec.ts
@@ -0,0 +1,21 @@
+import { shallowMount } from '@vue/test-utils'
+import AppHeader from '@/components/AppHeader.vue'
+
+// Test suite
+describe('AppHeader', () => {
+  it('If user is not logged in, do not show logout button', () => {
+    const wrapper = shallowMount(AppHeader)
+
+    expect(wrapper.find('button').isVisible()).toBe(false)
+  })
+
+  it('If user is logged in, show logout button', async () => {
+    const wrapper = shallowMount(AppHeader)
+
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.find('button').isVisible()).toBe(true)
+  })
+})
